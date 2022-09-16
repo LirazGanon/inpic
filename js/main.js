78430ea90fc9ccde7e1a1aabee404cdd9b39ce6f
LirazGanon <105818149+LirazGanon@users.noreply.github.com>
2022-09-16 23:57:48 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 'use strict'
 
-var gQuests = [
-    { id: 1, opts: ['Apple', 'Banna', 'Orange'], correctOptIndex: 1 },
-    { id: 2, opts: ['Kiwi', 'Lemon', 'Apple'], correctOptIndex: 0 },
-    { id: 3, opts: ['Banna', 'Strawberry', 'Cherry'], correctOptIndex: 2 },
-    { id: 4, opts: ['Melon', 'Orange', 'Leomon'], correctOptIndex: 1 },
-    { id: 5, opts: ['Orange', 'Melon', 'Apple'], correctOptIndex: 2 },
-    { id: 6, opts: ['Cherry', 'Banna', 'Pineapple'], correctOptIndex: 2 },
-    { id: 7, opts: ['Pomegranate', 'Cherry', 'Strawberry'], correctOptIndex: 0 },
-    { id: 8, opts: ['Strawberry', 'Cherry', 'Kiwi'], correctOptIndex: 0 },
-]
+var gQuests = []
 var worngClick = 0
 var gCurrQuestIdx = 0
 var gElContainer = document.querySelector('.q-container')
@@ -17,8 +8,25 @@ var gElContainer = document.querySelector('.q-container')
 function onInitGame() {
     gCurrQuestIdx = 0
     worngClick = 0
+    gElContainer.classList.remove("hide")
+    var elFinishDiv = document.querySelector('.finish')
+    elFinishDiv.classList.add("hide")
+    gQuests = createQuests()
     renderQuest()
 
+}
+
+function createQuests() {
+    return [
+        { id: 1, opts: ['תַּפּוּחַ', 'בָּנָנָה', 'תַּפּוּז'], correctOptIndex: 1 },
+        { id: 2, opts: ['קִיוִי', 'לִימוֹן', 'תַּפּוּחַ'], correctOptIndex: 0 },
+        { id: 3, opts: ['בָּנָנָה', 'תּוּת', 'דֻּבְדְּבָן'], correctOptIndex: 2 },
+        { id: 4, opts: ['מְלוֹן', 'תַּפּוּז', 'לִימוֹן'], correctOptIndex: 1 },
+        { id: 5, opts: ['תַּפּוּז', 'מְלוֹן', 'תַּפּוּחַ'], correctOptIndex: 2 },
+        { id: 6, opts: ['דֻּבְדְּבָן', 'בָּנָנָה', 'אָנָנָס'], correctOptIndex: 2 },
+        { id: 7, opts: ['רִימּוֹן', 'דֻּבְדְּבָן', 'תּוּת'], correctOptIndex: 0 },
+        { id: 8, opts: ['תּוּת', 'דֻּבְדְּבָן', 'קִיוִי'], correctOptIndex: 0 }
+    ]
 }
 
 function renderQuest() {
@@ -26,7 +34,7 @@ function renderQuest() {
     gElContainer.style.backgroundImage = `url('pic/${gCurrQuestIdx + 1}.jpg')`
     for (var i = 0; i < gQuests[gCurrQuestIdx].opts.length; i++) {
         var currOpts = gQuests[gCurrQuestIdx].opts[i]
-        var leftValue = 2 + i * 33
+        var leftValue = 2.8 + i * 33
         var elQuestsStr = `<div id="${i}" class="btn answer" style="left:${leftValue}%;" onclick="onOptClick(${i})" >${currOpts}</div>`
         strHtml += elQuestsStr
     }
@@ -40,7 +48,6 @@ function onOptClick(optIdx) {
     var elChooseDiv = document.getElementById(optIdx)
 
     if (optIdx === correctOptIndex) {
-        console.log('Yeaa!');
         elChooseDiv.style.backgroundColor = 'green'
         if (gCurrQuestIdx === gQuests.length - 1) {
             endGame()
@@ -62,17 +69,15 @@ function onOptClick(optIdx) {
 }
 
 function endGame() {
+
     var sound = new Audio('/pic/claps.wav')
     sound.play()
-    gElContainer.setAttribute("style", "background-image:url('pic/conf.gif');")
+    gElContainer.classList.add('hide')
+    var elMistakeSpan = document.querySelector('.mistakes')
+    elMistakeSpan.innerText = worngClick
     var msg = worngClick > 3 ? 'You can do Better!' : 'You did Great!'
-    var strHtml = `<h1>Congrats you finished the game!</h1>
-    <h2>You have ${worngClick} mistakes</h2>
-    <p>${msg}</p>
-    <div class="btn" onclick="onInitGame()">
-      Play Again
-        </div>`
-
-    gElContainer.innerHTML = strHtml
-
+    var elMsg = document.querySelector('.msg')
+    elMsg.innerText = msg
+    var elFinishDiv = document.querySelector('.finish')
+    elFinishDiv.classList.remove("hide")
 }
